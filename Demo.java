ba058115d4ae2ccf7fbf44142c43bf5560976cde
zhaigc <zhaigc@foxmail.com>
2018-05-18 15:20:01 +0800

    Add -Demo

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Demo World");
+		System.out.println("Demo zhaigc");
+		System.out.println("B");
+	}
+	
+}
